f737fb70981fcc790802f4ba9537212548fdaa87
Astrid <scarlettstrid@gmail.com>
2023-02-25 18:48:37 +0800

    learning react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+      <h5>Hello, I am</h5>
+      <h1>{props.name}</h1>
+      <h4>nice to meet you.</h4>
+      <hr width="100%"></hr>
+    </>
+  )
+}
+
+const ComponentPractice = () => {
+  const names = ['a','b','c','d']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="a" />
+      <Person name="aa" />
+      <Person name="aaa" />
+      <Person name="aaaa" />
     </div>
   );
+};
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(()=> {
+    setCounter(100)
+  }, [])
+  // setCounter(100)
+  return (
+    <div className="App">
+      <button onClick={()=>{setCounter((prev)=>prev+1)}}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>{setCounter((prev)=>prev-1)}}>-</button>
+    </div>
+  )
 }
 
 export default App;
+
